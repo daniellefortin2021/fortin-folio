5b21cc622927497fae4eb2f80120fd6d38ab4c25
Danielle Fortin <fortind6@gmail.com>
2022-04-07 21:16:23 -0400

    created about component

---
+++ fortin-folio/src/components/About/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+function About() {
+    return (
+      <div>
+        <div>
+            <h1 id="about">About Me</h1>
+            <p>With over five years of experience managing websites, developing and maintaining brands, managing social media, and developing communication strategies, I am a seasoned digital media strategist with a strong interest in working with small businesses and non-profit organizations. Based in Cleveland, OH, I am interested in helping companies and organizations build their communication plans and brands from the ground up. Outside of work I am a serial hobbyist. You can find me either rollerblading at the park, at home crocheting and watching a cheesy movie, taking my dog for a hike, or at the pottery studio.</p>
+        </div>
+        <div>
+            <div>
+                <h3>Web Design</h3>
+            </div>
+            <div>
+                <h3>Digital Media</h3>
+            </div>
+            <div>
+                <h3>Brand Management</h3>
+            </div>
+        </div>
+      </div>
+    
+    );
+  }
+  
+  export default About;
