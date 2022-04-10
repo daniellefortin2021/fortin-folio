5eaf171370aaae146305ae90f327344bf0d2624c
Danielle Fortin <fortind6@gmail.com>
2022-04-10 16:57:05 -0400

    updated contact form to handle changes

--- fortin-folio/src/components/pages/Contact.js
+++ fortin-folio/src/components/pages/Contact.js
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { validateEmail } from '../../utils/helpers'
 
 function Contact() {
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const {name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            if(!isValid) {
+                setErrorMessage('Your email is invalid');
+            } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                    setErrorMessage('');
+                }
+            }
+        }
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value })
+        }
+        console.log(errorMessage);
+    }
+
+    function handelSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <div className="header">
             <h2>Let's Keep in Touch!</h2>
-            <Form id="form"> 
-                <Form.Group controlId="formBasicEmail">
+            <Form id="form" onSubmit={handelSubmit}> 
+                <Form.Group>
+                    <Form.Label id="form-label">Name</Form.Label>
+                    <Form.Control id="form-input" type="name" placeholder="Your Name" defaultValue={name} onChange={handleChange}/>
+                </Form.Group>
+                <Form.Group>
                     <Form.Label id="form-label">Email address</Form.Label>
-                    <Form.Control id="form-input" type="email" placeholder="Enter email" />
+                    <Form.Control id="form-input" type="email" placeholder="Your email" defaultValue={email} onChange={handleChange}/>
                 </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label id="form-label">Password</Form.Label>
-                    <Form.Control id="form-input" type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group controlId="formBasicMessage">
+                <Form.Group>
                     <Form.Label id="form-label">Message</Form.Label>
-                    <Form.Control id="form-input" as="textarea" rows={3} type="message" placeholder="Message" />
+                    <Form.Control id="form-input" as="textarea" rows={3} type="message" placeholder="Message" defaultValue={message} onChange={handleChange} />
                 </Form.Group>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
                 <div className="form-button">
                     <Button id="button" variant="primary" type="submit">
                         Submit
