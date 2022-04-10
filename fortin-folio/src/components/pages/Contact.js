4357068c0a9503bd33de731d783c0275d923afcc
Danielle Fortin <fortind6@gmail.com>
2022-04-10 13:23:24 -0400

    updated contact form

--- fortin-folio/src/components/pages/Contact.js
+++ fortin-folio/src/components/pages/Contact.js
@@ -5,22 +5,24 @@ function Contact() {
     return (
         <div className="header">
             <h2>Let's Keep in Touch!</h2>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        I'll never share your email with anyone else.
-                    </Form.Text>
+            <Form id="form"> 
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label id="form-label">Email address</Form.Label>
+                    <Form.Control id="form-input" type="email" placeholder="Enter email" />
                 </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label id="form-label">Password</Form.Label>
+                    <Form.Control id="form-input" type="password" placeholder="Password" />
                 </Form.Group>
-                <Button id="button" variant="primary" type="submit">
-                    Submit
-                </Button>
+                <Form.Group controlId="formBasicMessage">
+                    <Form.Label id="form-label">Message</Form.Label>
+                    <Form.Control id="form-input" as="textarea" rows={3} type="message" placeholder="Message" />
+                </Form.Group>
+                <div className="form-button">
+                    <Button id="button" variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </div>
 
             </Form>
         </div>
