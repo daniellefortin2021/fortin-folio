e3765bede3856e68d9e0441f26aac28b9b3756bb
Danielle Fortin <fortind6@gmail.com>
2022-04-11 22:13:26 -0400

    updated source for home images

--- fortin-folio/src/components/pages/Home.js
+++ fortin-folio/src/components/pages/Home.js
@@ -6,11 +6,11 @@ function Home () {
     return (
         <Row className="home flex-row">
             <Col className="home-images">
-                <img className='headshot' src="../assets/danielle-fortin.png" alt="danielle fortin headshot"/>
-                <img className="home-daisy-1" src="../assets/daisy.png" alt="daisy vector"/>
+                <img className='headshot' src="assets/danielle-fortin.png" alt="danielle fortin headshot"/>
+                <img className="home-daisy-1" src="assets/daisy.png" alt="daisy vector"/>
             </Col>
             <Col className="home-images">
-                <img className="home-daisy-2" src="../assets/daisy.png" alt="daisy vector"/>
+                <img className="home-daisy-2" src="assets/daisy.png" alt="daisy vector"/>
                 <h1 id="title">Danielle Fortin</h1>
                 <h3>web design | digital marketing | brand development</h3>
             </Col>
