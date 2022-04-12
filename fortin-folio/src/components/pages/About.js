3dca49357975484ea0edd0703507ae1787f5302b
Danielle Fortin <fortind6@gmail.com>
2022-04-11 22:10:56 -0400

    updated about text

--- fortin-folio/src/components/pages/About.js
+++ fortin-folio/src/components/pages/About.js
@@ -11,7 +11,7 @@ function About() {
                 <p>With over five years of experience managing websites, developing and maintaining brands, managing social media, and developing communication strategies, I am a seasoned digital media strategist with a strong interest in working with small businesses and non-profit organizations. Based in Cleveland, OH, I am interested in helping companies and organizations build their communication plans and brands from the ground up. Outside of work I am a serial hobbyist. You can find me either rollerblading at the park, at home crocheting and watching a cheesy movie, taking my dog for a hike, or at the pottery studio.</p>
             </div>
             <div>
-                <Row className="skills">
+                <Row className="skills flex-row">
                     <Col>
                         <h3>Web Design</h3>
                         <p>I have over 5 years of experience managaing websites and one year of experience designing and developing websites for international non-profit organizations.</p>
